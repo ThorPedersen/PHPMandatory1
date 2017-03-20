c5b4c2a6e5e5a6850b5c00ee496b519e0a743713
Thor <technostamper@hotmail.com>
2017-03-20 09:23:32 +0100

    small db stuff

--- db_handler.php
+++ db_handler.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbc = mysqli_connect('localhost', 'root', '', 'mandatory');
+$conn = mysqli_connect("localhost", "root", "", "mandatory");
 
-if(!$dbc) {
+if(!$conn) {
 	echo "error: unable to connect to MySQL" . PHP_EOL;
 	//remove debbugging error when in production
 	echo "Debugging error" . mysqli_connect_error;
--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn = mysqli_connect("localhost", "root", "", "mandatory");
+require_once('db_handler.php');
 
 $message = "";
 /* check connection */
