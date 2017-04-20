a7502bc8b8140fd7253debe23fd0a71590e8dd2c
Thor <technostamper@hotmail.com>
2017-04-20 03:24:00 +0200

    blah

--- db_handler.php
+++ db_handler.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "mandatory");
+//$conn = mysqli_connect("localhost", "root", "", "mandatory");
+$conn = mysqli_connect("localhost", "root", "", "thor_php_mandatory_1");
 
 if(!$conn) {
 	echo "error: unable to connect to MySQL" . PHP_EOL;
